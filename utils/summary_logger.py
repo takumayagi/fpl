133dded0c516efac429b14c4a38c1d35a5b15824
Takuma Yagi <signull8192@gmail.com>
2018-06-05 17:45:33 +0900

    Fix bug, removed old code

--- utils/summary_logger.py
+++ utils/summary_logger.py
@@ -6,17 +6,19 @@
 #
 # Distributed under terms of the MIT license.
 
-import time
-import datetime
 import pandas as pd
 import subprocess
-import argparse
+from subprocess import CalledProcessError
 
 
 def get_commit_id():
     cmd = "git log -n 1 --format=%H"
-    out = subprocess.check_output(cmd.split(" ")).decode('utf-8')
+    try:
+        out = subprocess.check_output(cmd.split(" ")).decode('utf-8')
+    except CalledProcessError:
+        return ""
     return out.rstrip("\r\n")
+
 
 class SummaryLogger(object):
 
@@ -34,12 +36,12 @@ class SummaryLogger(object):
         for key, value in update_dict.items():
             self._summ[key] = value
 
-    def update_by_cond(self, key, value, time, cond="lower"):
+    def update_by_cond(self, key, value, timing, cond="lower"):
         assert cond == "lower" or cond == "higher"
         sign = 1 if cond == "lower" else -1
         if key not in self._summ or sign * value < sign * self._summ[key]:
             self._summ[key] = value
-            self._summ[key+"_tm"] = time
+            self._summ[key+"_tm"] = timing
 
     def write(self):
         """Write summary to csv file
@@ -57,44 +59,3 @@ class SummaryLogger(object):
         else:
             current = current.append(new_summary)
         current.to_csv(self._output_path)
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cnn', type=str, default="CNN_M_2048")
-    parser.add_argument('--optimizer', type=str, default="sgd")
-    parser.add_argument('--batchsize', '-b', type=int, default=256)
-    args = parser.parse_args()
-    print(vars(args))
-
-    summary_dict = initialize_summary(args, "log.txt")
-    date = datetime.datetime.now()
-    date_str = date.strftime('%y%m%d_%H%M%S')
-    write_summary("summary.csv", summary_dict, date_str)
-    summary_dict["added"] = 1
-    write_summary("summary.csv", summary_dict, date_str)
-    time.sleep(2)
-
-    loss = 1.0
-    acc = 0.5
-    update_loss_acc(summary_dict, loss, acc, 1)
-    loss = 0.5
-    acc = 0.4
-    update_loss_acc(summary_dict, loss, acc, 2)
-    loss = [1.0, 0.5]
-    acc = [0.4, 0.6]
-    update_loss_acc(summary_dict, loss, acc, 2)
-
-    print(summary_dict)
-    assert summary_dict["loss"] == 0.5
-    assert summary_dict["acc"] == 0.5
-    assert summary_dict["loss_ep"] == 2
-    assert summary_dict["acc_ep"] == 1
-    assert summary_dict["loss_1_ep"] == 2
-    assert summary_dict["loss_1"] == 1.0
-    assert summary_dict["acc_2"] == 0.6
-
-    date = datetime.datetime.now()
-    date_str = date.strftime('%y%m%d_%H%M%S')
-    write_summary("summary.csv", summary_dict, date_str)
